fec0883c15f3aeda16481b391e6b0b9d18639ab4
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-04-06 19:18:09 +0800

    nixos: 2025-04-06 19:18:09

--- flake.nix
+++ flake.nix
@@ -5,8 +5,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
-      # IMPORTANT: we're using "libgbm" and is only available in unstable so ensure
-      # to have it up to date or simply don't specify the nixpkgs input  
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -15,11 +13,7 @@
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
-      modules = [
-        # 这里导入之前我们使用的 configuration.nix，
-        # 这样旧的配置文件仍然能生效
-        ./configuration.nix
-      ];
+      modules = [ ./configuration.nix ];
     };
   };
 }
