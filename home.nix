8d1b9aed6520590892fb89dd2f1dec5c0f8a26bb
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-04-07 00:01:30 +0800

    nixos: 2025-04-07 00:01:30

--- home.nix
+++ home.nix
@@ -42,7 +42,7 @@
     #
     # # utils
     # ripgrep # recursively searches directories for a regex pattern
-    # jq # A lightweight and flexible command-line JSON processor
+    jq # A lightweight and flexible command-line JSON processor
     # yq-go # yaml processor https://github.com/mikefarah/yq
     # eza # A modern replacement for ‘ls’
     # fzf # A command-line fuzzy finder
