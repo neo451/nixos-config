c7230296e3502ef1d5cb45aa21aa01db2fd56564
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-04-07 13:04:41 +0800

    nixos: 2025-04-07 13:04:41

--- home.nix
+++ home.nix
@@ -190,7 +190,8 @@
     settings = {
       theme = "catppuccin-mocha";
       font-size = 10;
-      keybind = [ "ctrl+h=goto_split:left" "ctrl+l=goto_split:right" ];
+      # keybind = [ "ctrl+h=goto_split:left" "ctrl+l=goto_split:right" ];
+      command = "fish";
     };
   };
 
