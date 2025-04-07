516c95feb57168ef43e0aa1c8156cdbd731f68ac
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-04-08 01:23:18 +0800

    nixos: 2025-04-08 01:23:18

--- flake.nix
+++ flake.nix
@@ -18,32 +18,31 @@
     nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
   };
 
-  outputs =
-    { self, nixpkgs, system, home-manager, nix-doom-emacs, ... }@inputs: {
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            environment.systemPackages = let
-              doom-emacs = nix-doom-emacs.packages.${system}.default.override {
-                doomPrivateDir = ./doom.d;
-              };
-            in [ doom-emacs ];
-          }
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
+  outputs = { self, nixpkgs, home-manager, nix-doom-emacs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem rec {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          environment.systemPackages = let
+            doom-emacs = nix-doom-emacs.packages.${system}.default.override {
+              doomPrivateDir = ./doom.d;
+            };
+          in [ doom-emacs ];
+        }
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
 
-            home-manager.users.n451 = { imports = [ ./home.nix ]; };
+          home-manager.users.n451 = { imports = [ ./home.nix ]; };
 
-            home-manager.extraSpecialArgs = inputs;
+          home-manager.extraSpecialArgs = inputs;
 
-            home-manager.backupFileExtension = "backup";
-          }
-        ];
-      };
+          home-manager.backupFileExtension = "backup";
+        }
+      ];
     };
+  };
 }
