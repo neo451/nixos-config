13e00243d69dde5113a5a811be71d7d3896701f7
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-04-08 01:18:26 +0800

    nixos: 2025-04-08 01:18:26

--- flake.nix
+++ flake.nix
@@ -33,8 +33,6 @@
             imports = [ ./home.nix nix-doom-emacs.hmModule ];
           };
 
-          # 使用 home-manager.extraSpecialArgs 自定义传递给 ./home.nix 的参数
-          # 取消注释下面这一行，就可以在 home.nix 中使用 flake 的所有 inputs 参数了
           home-manager.extraSpecialArgs = inputs;
 
           home-manager.backupFileExtension = "backup";
