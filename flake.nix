c4a168baf0684854f29bd1b46be8def6f0ceb158
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-04-07 15:32:18 +0800

    nixos: 2025-04-07 15:32:18

--- flake.nix
+++ flake.nix
@@ -5,10 +5,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
-      # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nur = {
