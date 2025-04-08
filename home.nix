c8bcaa46d4c13c279908e5f78b5bdf6249ee9bf9
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-04-08 14:00:44 +0800

    nixos: 2025-04-08 14:00:44

--- home.nix
+++ home.nix
@@ -134,7 +134,7 @@
     # nmap # A utility for network discovery and security auditing
     # ipcalc # it is a calculator for the IPv4/v6 addresses
     #
-    # # misc
+    # misc
     cowsay
     file
     which
