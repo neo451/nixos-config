d83428d487b69c6146b0a53cf097ba6fcab89fe2
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-04-08 22:45:26 +0800

    nixos: 2025-04-08 22:45:26

--- configuration.nix
+++ configuration.nix
@@ -100,6 +100,13 @@
     };
   };
 
+  programs.hyprland = {
+    # Install the packages from nixpkgs
+    enable = true;
+    # Whether to enable XWayland
+    xwayland.enable = true;
+  };
+
   programs.steam = {
     enable = true;
     remotePlay.openFirewall =
