b00ec32ff4739dfeee2e1f0d30ac7dfcbc5b0421
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-04-25 20:42:21 +0800

    nixos: 2025-04-25 20:42:21

--- home/fish.nix
+++ home/fish.nix
@@ -2,11 +2,11 @@
   programs.fish = {
     enable = true;
     shellAliases = {
-      vi = "nvim";
+      # vi = "nvim";
       cat = "bat";
       em = "emacs -nw";
-      mvi =
-        "cd ~/Clone/neovim/ && VIMRUNTIME=./runtime ./build/bin/nvim --luamod-dev";
+      vi =
+        "VIMRUNTIME=~/Clone/neovim/runtime ~/Clone/neovim/build/bin/nvim --luamod-dev";
     };
     shellAbbrs = {
       nrs = "~/scripts/rebuild-os";
