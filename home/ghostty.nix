1aa9d2eb1ccd14558e9e4c8ce75334d639e17dc1
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-05-26 17:49:50 +0800

    nixos: 2025-05-26 17:49:50

--- home/ghostty.nix
+++ home/ghostty.nix
@@ -3,7 +3,8 @@
     enable = true;
     enableFishIntegration = true;
     settings = {
-      theme = "catppuccin-mocha";
+      # theme = "catppuccin-mocha";
+      theme = "pywal";
       font-size = 10;
       keybind = [
         # "ctrl+h=goto_split:left" "ctrl+l=goto_split:right" 
