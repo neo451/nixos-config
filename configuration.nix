8bcd14142991ff71f24ea49dd7b1b987587c5f2a
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-06-09 19:33:01 +0800

    nixos: 2025-06-09 19:33:01

--- configuration.nix
+++ configuration.nix
@@ -118,42 +118,35 @@
   # Enable OpenGL
   hardware.graphics = { enable = true; };
 
-  hardware.nvidia.package =
-    config.boot.kernelPackages.nvidiaPackages.stable; # Same as production
+  hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
 
-  # Load nvidia driver for Xorg and Wayland
-  # services.xserver.videoDrivers = [ "nvidia" ];
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
 
-  # hardware.nvidia = {
-  #
-  #   # Modesetting is required.
-  #   modesetting.enable = true;
-  #
-  #   # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-  #   # Enable this if you have graphical corruption issues or application crashes after waking
-  #   # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-  #   # of just the bare essentials.
-  #   powerManagement.enable = false;
-  #
-  #   # Fine-grained power management. Turns off GPU when not in use.
-  #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-  #   powerManagement.finegrained = false;
-  #
-  #   # Use the NVidia open source kernel module (not to be confused with the
-  #   # independent third-party "nouveau" open source driver).
-  #   # Support is limited to the Turing and later architectures. Full list of 
-  #   # supported GPUs is at: 
-  #   # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-  #   # Only available from driver 515.43.04+
-  #   open = false;
-  #
-  #   # Enable the Nvidia settings menu,
-  #   # accessible via `nvidia-settings`.
-  #   nvidiaSettings = true;
-  #
-  #   # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
-  # };
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = true;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    open = true;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 
   programs.steam = {
     enable = true;
@@ -173,6 +166,7 @@
 
   services = {
     xserver = {
+      videoDrivers = [ "nvidia" ];
       windowManager.awesome = {
         # enable = true;
         luaModules = with pkgs.luaPackages; [
