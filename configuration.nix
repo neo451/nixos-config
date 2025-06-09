dbce488e9473c757d9057b413643bea1d37d1051
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-06-09 19:43:21 +0800

    nixos: 2025-06-09 19:43:21

--- configuration.nix
+++ configuration.nix
@@ -146,6 +146,11 @@
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    prime = {
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
 
   programs.steam = {
