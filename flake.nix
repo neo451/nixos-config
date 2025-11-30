7dfe755b91e40a93b7239c328512591a43b41b26
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-11-30 00:54:52 +0000

    nixos: 2025-11-30 00:54:52

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
     ...
   } @ inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      stdenv.hostPlatform.system = "x86_64-linux";
       specialArgs = {inherit inputs;};
       modules = [
         inputs.musnix.nixosModules.musnix
