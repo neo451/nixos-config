8248ba309fd8b2ca8a6c420fe92d99f15443be33
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-12-03 10:15:48 +0000

    nixos: 2025-12-03 10:15:48

--- home/ghostty.nix
+++ home/ghostty.nix
@@ -8,7 +8,7 @@
       background-opacity = 0.8;
       font-size = 10;
       keybind = [
-        # "ctrl+h=goto_split:left" "ctrl+l=goto_split:right" 
+        # "ctrl+h=goto_split:left" "ctrl+l=goto_split:right"
         "ctrl+shift+c=unbind"
       ];
       command = "fish";
