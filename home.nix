e7674e6e90c8a755a6c733dcb6211b2127ae747a
zizhou teng (n451) <2020200706@ruc.edu.cn>
2026-02-20 21:29:08 +0000

    nixos: 2026-02-20 21:29:08

--- home.nix
+++ home.nix
@@ -9,6 +9,18 @@
     ./home/ghostty.nix
     ./home/newsboat.nix
   ];
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "image/jpeg" = "org.gnome.Loupe.desktop";
+      "image/png" = "org.gnome.Loupe.desktop";
+      "image/gif" = "org.gnome.Loupe.desktop";
+      "image/webp" = "org.gnome.Loupe.desktop";
+      "image/tiff" = "org.gnome.Loupe.desktop";
+      "image/bmp" = "org.gnome.Loupe.desktop";
+      "image/svg+xml" = "org.gnome.Loupe.desktop";
+    };
+  };
 
   home.username = "n451";
   home.homeDirectory = "/home/n451";
