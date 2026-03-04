80d580f6fe7b123cb74179cc22ab992892ba506f
zizhou teng (n451) <2020200706@ruc.edu.cn>
2026-03-04 00:03:22 +0000

    nixos: 2026-03-04 00:03:22

--- home.nix
+++ home.nix
@@ -15,10 +15,6 @@
       "image/jpeg" = "org.gnome.Loupe.desktop";
       "image/png" = "org.gnome.Loupe.desktop";
       "image/gif" = "org.gnome.Loupe.desktop";
-      "image/webp" = "org.gnome.Loupe.desktop";
-      "image/tiff" = "org.gnome.Loupe.desktop";
-      "image/bmp" = "org.gnome.Loupe.desktop";
-      "image/svg+xml" = "org.gnome.Loupe.desktop";
     };
   };
 
