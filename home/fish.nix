0a6c8865eb36dd9dcfc48626a4ad9e9bd305aff7
zizhou teng (n451) <2020200706@ruc.edu.cn>
2026-05-20 23:40:45 +0100

    nixos: 2026-05-20 23:40:45

--- home/fish.nix
+++ home/fish.nix
@@ -19,9 +19,6 @@
       if test -f ~/secret/SPOTIFY.fish
           source ~/secret/SPOTIFY.fish
       end
-      function rpi-imager-root
-        sudo env QT_QPA_PLATFORM=wayland WAYLAND_DISPLAY="$WAYLAND_DISPLAY" XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR" DBUS_SESSION_BUS_ADDRESS="$DBUS_SESSION_BUS_ADDRESS" rpi-imager $argv
-      end
       zoxide init fish | source
     '';
     shellAbbrs = {
