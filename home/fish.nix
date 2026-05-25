62ed2b37c02f16b115b4232aa7079c3c821acc3e
zizhou teng (n451) <2020200706@ruc.edu.cn>
2026-05-25 23:43:50 +0100

    nixos: 2026-05-25 23:43:50

--- home/fish.nix
+++ home/fish.nix
@@ -26,20 +26,11 @@
     shellAbbrs = {
       nfu = "cd ~/nixos-config; sudo nix flake update";
       nrs = "~/scripts/rebuild-os";
-      pushn = "~/scripts/push-notes";
-      gss = "git status --short";
-      ga = "git add";
       gd = "git diff --output-indicator-new=' ' --output-indicator-old=' '";
-      gc = "git commit";
-      gp = "git push";
-      gP = "git pull";
-      gl = "git log";
-      gb = "git branch";
-      gi = "git init";
-      gcl = "git clone";
       em = "emacs -nw";
       cd = "z";
       lg = "lazygit";
+      ls = "eza -lah";
     };
   };
 }
